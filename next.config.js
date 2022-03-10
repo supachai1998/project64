153c666fd46431773af8250fe49d822e8be7b0bc
supcahai <supcahai_thng@hotmail.com>
2022-03-10 20:18:42 +0700

    Fix Error: Cannot find module @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,44 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-module.exports = withBundleAnalyzer({
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: false,
+//   images: {
+//     domains:["images.unsplash.com","shopee.co.th",],
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+//     minimumCacheTTL: 360,
+//   },
+//   experimental: { esmExternals: true,css: true,compress: true, },
+//   httpAgentOptions: {
+//     keepAlive: false
+//   },
+//   webpack: (config, {
+//     buildId,
+//     dev,
+//     isServer,
+//     defaultLoaders,
+//     webpack
+// }) => {
+  
+//     // Note: we provide webpack above so you should not `require` it
+//     // Perform customizations to webpack config
+//     config.plugins.push(
+//         new webpack.ProvidePlugin({
+//             $: "jquery",
+//             jQuery: "jquery",
+//             "window.jQuery": "jquery"
+//         })
+//     );
+//     // Important: return the modified config
+//     return config;
+// }
+// }
+// )
+
+
+module.exports = ({
   reactStrictMode: false,
   images: {
     domains:["images.unsplash.com","shopee.co.th",],
