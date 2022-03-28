18aa1243e856c93058104bb8d278d5ade8c85d6b
supcahai <supcahai_thng@hotmail.com>
2022-03-28 18:29:26 +0700

    fix node env build failed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { parse } = require('url')
 const next = require('next')
 const path = require('path')
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = false
 const hostname = '0.0.0.0'
 const port = 3000
 // when using middleware `hostname` and `port` must be provided below
