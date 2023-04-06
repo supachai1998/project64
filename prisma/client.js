43bddbde5106fa383b6e07104c9ef377e52f18a3
supachai phobpimai <supachai.ph@digitalsetup.co>
2023-04-06 09:02:48 +0700

    vercel

--- prisma/client.js
+++ prisma/client.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 // Avoid instantiating too many instances of Prisma in development
 // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
-let prisma
+let prisma 
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
@@ -12,6 +12,18 @@ if (process.env.NODE_ENV === 'production') {
   }
   prisma = global.prisma
 }
+
+if (prisma==null) prisma = new PrismaClient(
+    {
+        log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        ],
+    }
+)
+
 
 export default prisma
 
